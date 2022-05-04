a34cbabac70ffb18a7d9f664f4d598aecbff098f
separk <separk@businessinsight.co.kr>
2022-05-04 16:07:33 +0900

    Initial Commit

---
+++ src/main/java/mongo/crud/mongodb_transaction/MongodbTransactionApplication.java
@@ -0,0 +1,13 @@
+package mongo.crud.mongodb_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mongo/crud/mongodb_transaction/MongodbTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package mongo.crud.mongodb_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
